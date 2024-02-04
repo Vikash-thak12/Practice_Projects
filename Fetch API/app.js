599a352c6e063477a41e4a0cd767b4ddfb85b96c
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-04 21:47:51 +0530

    Added a function that used to receive data

--- Fetch API/app.js
+++ Fetch API/app.js
@@ -1,1 +1,20 @@
 url = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
+getdata();
+
+async function getdata() {
+    try {
+
+        const response = await fetch(url);
+        if(!response.ok)
+        {
+            throw new Error("Could not fetch the data: ");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
