45f76eeac6735f79236ec7fdfbc81e3246d5cfd8
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-07 19:47:21 +0530

    Final commit

--- colorChanger/app.js
+++ colorChanger/app.js
@@ -1,5 +1,28 @@
-let rect = document.querySelector("#center");
+var rect = document.querySelector("#center");
 
-rect.addEventListener("mousemove", () => {
-   console.log( rect.getBoundingClientRect());
+rect.addEventListener("mousemove", (details) => {
+   var rectLocation = rect.getBoundingClientRect();
+   var insiderectval = details.clientX - rectLocation.left;
+
+   if(insiderectval < rectLocation.width/2) {
+    var redcolor = gsap.utils.mapRange(0, rectLocation.width/2, 255, 0, insiderectval);
+    gsap.to(rect, {
+        backgroundColor: `rgb(${redcolor},0,0)`,
+        ease: Power4,
+    });
+   } else {
+    var bluecolor = gsap.utils.mapRange(rectLocation.width/2, rectLocation.width, 0, 255, insiderectval);
+    gsap.to(rect, {
+        backgroundColor: `rgb(0,0,${bluecolor})`,
+        ease: Power4,
+    });
+   }
 })
+
+rect.addEventListener("mouseleave", () => {
+    gsap.to(rect, {
+        backgroundColor: "white"
+    })
+})
+
+// console.log(gsap.utils.mapRange(0, rectLocation.width/2, 255, 0, insiderectval));
