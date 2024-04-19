d790afc6ab4be6897f7ac877540010e9558b7815
Vikash Thakur <thakvikash622@gmail.com>
2024-04-20 00:56:33 +0530

    Done with the js

--- Gsap_rect_Mover/app.js
+++ Gsap_rect_Mover/app.js
@@ -1,3 +1,23 @@
 window.addEventListener("mousemove", function(details) {
     // console.log(details.clientX)
+    let rect = document.querySelector("#rect")
+    
+    let xval = gsap.utils.mapRange(0,
+        window.innerWidth,
+        100+rect.getBoundingClientRect().width/2,
+        window.innerWidth-(100+rect.getBoundingClientRect().width/2),
+        details.clientX)
+    gsap.to("#rect", {
+        left: xval + "px",
+        ease: Power3
+    })
 })
+
+
+// let rect = document.querySelector("#rect")
+// rect.addEventListener("mousemove", function(details) {
+//     gsap.to(rect, {
+//         left: details.clientX + "px",
+//         ease: Power3
+//     })
+// })
