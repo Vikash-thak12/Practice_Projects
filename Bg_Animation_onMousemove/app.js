4bf06fddf80cb98f912a88e86a501c8872bcd38d
Vikash Thakur <thakvikash622@gmail.com>
2024-04-26 23:40:06 +0530

    Created another function for gaping between the icons

--- Bg_Animation_onMousemove/app.js
+++ Bg_Animation_onMousemove/app.js
@@ -1,6 +1,28 @@
-function createHearts() {
+let prevMouseX = null;
+let prevMouseY = null;
+
+function heart(event) {
+    if(prevMouseX !== null && prevMouseY !== null) {
+        let deltaX = event.pageX - prevMouseX;
+        let deltaY = event.pageY - prevMouseY;
+
+        let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+        if(distance >= 100) {
+            createHearts(event.pageX, event.pageY)
+            prevMouseX = event.pageX;
+            prevMouseY = event.pageY;
+        }
+    }
+}
+
+
+function createHearts(x,y) {
     let i = document.createElement('i');
     document.body.appendChild(i)
+
+    i.style.top = (event.pageY) +  'px';
+    i.style.left = (event.pageX) + 'px';
 
     setTimeout(() => {
         document.body.removeChild(i)
