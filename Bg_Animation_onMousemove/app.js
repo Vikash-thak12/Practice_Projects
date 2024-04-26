ade93be6092f2952a305f2a6cca9835947113a3b
Vikash Thakur <thakvikash622@gmail.com>
2024-04-26 23:50:32 +0530

    Customize the size of the icons by using the scale property

--- Bg_Animation_onMousemove/app.js
+++ Bg_Animation_onMousemove/app.js
@@ -2,34 +2,35 @@ let prevMouseX = null;
 let prevMouseY = null;
 
 function heart(event) {
-    if(prevMouseX !== null && prevMouseY !== null) {
+    if (prevMouseX !== null && prevMouseY !== null) {
         let deltaX = event.pageX - prevMouseX;
         let deltaY = event.pageY - prevMouseY;
 
         let distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
 
-        if(distance >= 100) {
-            createHearts(event.pageX, event.pageY)
-            prevMouseX = event.pageX;
-            prevMouseY = event.pageY;
-        } else {
+        if (distance >= 100) {
             createHearts(event.pageX, event.pageY)
             prevMouseX = event.pageX;
             prevMouseY = event.pageY;
         }
+    } else {
+        createHearts(event.pageX, event.pageY)
+        prevMouseX = event.pageX;
+        prevMouseY = event.pageY;
     }
 }
 
 
-function createHearts(x,y) {
+function createHearts(x, y) {
     let i = document.createElement('i');
     document.body.appendChild(i)
 
-    i.style.top = (event.pageY) +  'px';
+    i.style.top = (event.pageY) + 'px';
     i.style.left = (event.pageX) + 'px';
+    i.style.scale = `${Math.random() * 1.5 + 1}`
 
     setTimeout(() => {
-        document.body.removeChild(i)    
+        document.body.removeChild(i)
     }, 1000);
 }
 
