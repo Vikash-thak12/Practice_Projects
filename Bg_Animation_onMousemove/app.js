c73838c90453c26d202865a519ce34563e7903a4
Vikash Thakur <thakvikash622@gmail.com>
2024-04-26 23:36:02 +0530

    added style to the icon

--- Bg_Animation_onMousemove/app.js
+++ Bg_Animation_onMousemove/app.js
@@ -1,6 +1,10 @@
 function createHearts() {
     let i = document.createElement('i');
     document.body.appendChild(i)
+
+    setTimeout(() => {
+        document.body.removeChild(i)
+    }, 2000);
 }
 
 document.addEventListener('mousemove', createHearts)
