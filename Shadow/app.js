5dc04bbbabd29f9a0e183c85ae1527a1bfd26d96
Vikash Thakur <thakvikash622@gmail.com>
2024-04-29 23:14:21 +0530

    Adding comments for clarification

--- Shadow/app.js
+++ Shadow/app.js
@@ -2,16 +2,19 @@ let text = document.querySelector("#text")
 let light = document.querySelector("#light")
 
 document.addEventListener("mousemove", function(event) {
+    // This will create the position of the light 
     let MouseX = event.clientX;
     let MouseY = event.clientY;
     light.style.left = MouseX + 'px';
     light.style.top = MouseY + 'px';
 
 
+    // This will create distance between the lights and the text
     let distanceX = MouseX - text.offsetLeft - text.offsetWidth / 2;
     let distanceY = MouseY - text.offsetTop - text.offsetHeight / 2;
 
 
+    // This is for the shadow of the text
     let newshadow = '';
     for(var i=0; i<200; i++) {
         let shadowX = -distanceX * (i / 200);
@@ -19,5 +22,6 @@ document.addEventListener("mousemove", function(event) {
         let opacity = 1 - ( i / 150);
         newshadow += (newshadow ? ',' : '') + shadowX + 'px ' + shadowY + 'px 0 rgba(33,33,33,'+opacity+')';
     }
+    // Adding shadow to the text
     text.style.textShadow = newshadow;
 })
