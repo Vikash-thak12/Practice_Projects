0bcd12b5a36c195dc95faa8630aeb20b385d82b4
Vikash Thakur <thakvikash622@gmail.com>
2024-04-30 19:33:23 +0530

    Final commit

--- Animated_Switch/app.js
+++ Animated_Switch/app.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     let boxes = document.querySelectorAll(".box");
     boxes.forEach( (box) => {
         let spans = [];
-        for(let i=0; i<40; i++) {
+        for(let i=0; i<100; i++) {
             let span = document.createElement('span');
             spans.push(span);
             box.appendChild(span)
+            span.style.top = `${i * 1}px`;
+            let randomDelay = (Math.random() * 0.25) + 0;
+            span.style.transitionDelay = randomDelay + 's';
         }
         box.addEventListener("click", function() {
             box.classList.toggle('active')
