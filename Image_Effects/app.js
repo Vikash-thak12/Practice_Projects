8da14dacab4291d56ad6d75dc91be1c2987378c0
Vikash Thakur <thakvikash622@gmail.com>
2024-05-03 23:25:42 +0530

    Working on the functionality of the box

---
+++ Image_Effects/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let boxes = document.querySelectorAll('.box');
+    boxes.forEach(function(box) {
+        let spans = [];
+        for(let i=0; i<200; i++) {
+            let span = document.createElement('span');
+            spans.push(span);
+            box.appendChild(span)
+        }
+    })
+})
