76fa12f2e0c8fa5dff531587ea0e68b60f653ac3
Vikash Thakur <thakvikash622@gmail.com>
2024-05-03 23:39:32 +0530

    The effect is working properly

--- Image_Effects/app.js
+++ Image_Effects/app.js
@@ -6,6 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
             let span = document.createElement('span');
             spans.push(span);
             box.appendChild(span)
+            span.style.top = `${i * 1}px`;
+            span.style.transitionDelay = (Math.random() * 1.5) +  0 + 's';
         }
+        box.addEventListener('click', function() {
+            box.classList.toggle('active');
+            spans.forEach(function(span) {
+                span.style.setProperty('--x', Math.floor(Math.random() * 400 -200) + 'px');
+                span.style.setProperty('--r', Math.floor(Math.random() * 360 - 0) + 'deg');
+            })
+        })
     })
 })
