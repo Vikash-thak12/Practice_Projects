27e222b1137877742b17b80ebe50d3802de60fde
Vikash Thakur <thakvikash622@gmail.com>
2024-05-04 23:09:25 +0530

    Gave comment

--- Magic_Indicator/app.js
+++ Magic_Indicator/app.js
@@ -2,7 +2,9 @@ let marker = document.querySelector("#marker");
 let item = document.querySelectorAll(".box");
 
 function indicator(e) {
+    //this is for the movemnet on the y-axis
     marker.style.top = e.offsetTop+'px';
+    //this is for the movement on the x-axis
     marker.style.left = e.offsetLeft+'px';
 }
 
