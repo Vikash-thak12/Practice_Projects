26c17bea4282cc1b07d1fd38d712d98ff22582bd
Vikash Thakur <thakvikash622@gmail.com>
2024-05-22 23:53:44 +0530

    The movement are working

---
+++ MagneticEffect/app.js
@@ -0,0 +1,17 @@
+let cards = document.querySelectorAll('.card')
+cards.forEach(card => {
+    card.addEventListener("mousemove", (e) => {
+        let x = e.offsetX;
+        let y = e.offsetY;
+
+        let cardwidth = card.clientWidth;
+        let cardHeight = card.clientHeight;
+
+        let posX = (x - cardwidth/2);
+        let posY = (y - cardHeight/2);
+
+        card.style.transform = `translateX(${posX}px) translateY(${posY}px)`
+
+
+    })
+})
