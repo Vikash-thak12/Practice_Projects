eb91dd4e06e9e2984c20dba080866a7cfb1dbfb6
Vikash Thakur <thakvikash622@gmail.com>
2024-06-07 23:09:28 +0530

    Updating the code

--- Background_Number_Effect/app.js
+++ Background_Number_Effect/app.js
@@ -1,10 +1,19 @@
-let numbers = document.querySelector(".numbers")
+let numbers = document.querySelector(".numbers");
 let numstring = numbers.textContent;
 let splitnum = numstring.split("");
 
-
 numbers.textContent = "";
 
-for(let i=0; i<splitnum.length; i++) {
+for (let i = 0; i < splitnum.length; i++) {
     numbers.innerHTML += "<span>" + splitnum[i] + "</span>";
 }
+
+let box = document.querySelector(".box");
+
+box.addEventListener("mouseenter", () => {
+    numbers.classList.add("no-animation");
+});
+
+box.addEventListener("mouseleave", () => {
+    numbers.classList.remove("no-animation");
+});
