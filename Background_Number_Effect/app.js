e37bf28454bb6562ee19d2375a4846ed612892bc
Vikash Thakur <thakvikash622@gmail.com>
2024-06-08 12:26:10 +0530

    Final commit

--- Background_Number_Effect/app.js
+++ Background_Number_Effect/app.js
@@ -7,13 +7,3 @@ numbers.textContent = "";
 for (let i = 0; i < splitnum.length; i++) {
     numbers.innerHTML += "<span>" + splitnum[i] + "</span>";
 }
-
-let box = document.querySelector(".box");
-
-box.addEventListener("mouseenter", () => {
-    numbers.classList.add("no-animation");
-});
-
-box.addEventListener("mouseleave", () => {
-    numbers.classList.remove("no-animation");
-});
